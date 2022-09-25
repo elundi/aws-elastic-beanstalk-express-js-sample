158cbec50ccc4a97c58a27c034c28a0189989703
elundi <emmanuel.lundi29@gmail.com>
2022-09-24 20:33:49 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bonjour Emmanuel LUNDI'));
+app.get('/', (req, res) => res.send('Bonjour, je suis Emmanuel LUNDI'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
